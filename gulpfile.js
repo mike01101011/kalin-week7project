e021ae4fa7dc830c53b713f38814e18a6ba98bbf
mike01101011 <mike01101011@gmail.com>
2015-10-27 17:00:38 -0400

    browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,23 @@ var gulp   = require('gulp'),
 		autoprefixer = require('gulp-autoprefixer'),
 		concat = require('gulp-concat');
 
-gulp.task('default', ['styles', 'watch']);
+// browserSync browserSync browserSync browserSync browserSync browserSync		
+
+		browserSync = require('browser-sync'),
+		reload = browserSync.reload;
+
+gulp.task('browserSync', function(){
+	browserSync.init({
+		proxy: 'http://localhost:8888/kalin-week7project/'
+	});
+});
+
+// browserSync browserSync browserSync browserSync browserSync browserSync
+// just addition of 'browserSync', 
+
+gulp.task('default', ['browserSync', 'styles', 'watch']);
+
+// browserSync browserSync browserSync browserSync browserSync browserSync
 
 gulp.task('styles', function() {
 	return gulp.src('sass/**/*.scss')
@@ -12,10 +28,24 @@ gulp.task('styles', function() {
 			errLogToConsole: true
 		}))
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('.'));
+		.pipe(gulp.dest('.'))
+
+// browserSync browserSync browserSync browserSync browserSync browserSync
+
+		.pipe(reload({ stream: true }));
+
+// browserSync browserSync browserSync browserSync browserSync browserSync
+
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
 	gulp.watch('sass/**/*.scss', ['styles']);
+
+// browserSync browserSync browserSync browserSync browserSync browserSync
+
+	gulp.watch('**/*.php', reload);	
+
+// browserSync browserSync browserSync browserSync browserSync browserSync
+
 });
--- header.php
+++ header.php
@@ -23,7 +23,6 @@
         <?php bloginfo( 'name' ); ?>
       </a>
     </h1>
-
     <?php wp_nav_menu( array(
       'container' => false,
       'theme_locations' => 'primary'
